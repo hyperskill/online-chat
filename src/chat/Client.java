da7a362dfdea927e992f63819fccbfd48ec4a101
AlinaOktyabrskaya <44409263+AlinaOktyabrskaya@users.noreply.github.com>
2018-10-24 17:50:14 +0300

    Stage#2: Client to server

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,7 +1,17 @@
 package chat;
+import java.io.*;
+import java.net.*;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Client started!");
+        String address = "127.0.0.1";
+int port = 23456;
+Socket socket = new Socket(InetAddress.getByName(address), port);
+DataInputStream input = new DataInputStream(socket.getInputStream());
+DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+        output.writeUTF("Hello from Client!");
+        System.out.println(input.readUTF());
+        
     }
 }
