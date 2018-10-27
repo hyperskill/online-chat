7e30f6850446e9d62af15968799212ae2e7ea5a1
AlinaOktyabrskaya <44409263+AlinaOktyabrskaya@users.noreply.github.com>
2018-10-28 00:27:03 +0300

    Update Server.java

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,7 +1,34 @@
 package chat;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.Time;
+
 public class Server {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         System.out.println("Server started!");
+
+        String address = "127.0.0.1";
+        int port = 9103;
+        int port2 = 9104;
+
+        ServerSocket server = new ServerSocket(port, 50,  InetAddress.getByName(address));
+        Socket socket = server.accept();
+        ServerSocket serverSocket = new ServerSocket(port2,50, InetAddress.getByName(address));
+        Socket socket1 = serverSocket.accept();
+
+            ServerThread thread = new ServerThread(socket);
+
+        ServerThread thread1 = new ServerThread(socket1);
+
+        thread.start();
+
+        thread1.start();
+
+
     }
 }
+
+
