9094e50b4313743603c4a03adb70cb81081682ec
ali-aa <alanplex@mail.ru>
2018-11-29 14:02:53 +0300

    Update Server.java

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,7 +1,17 @@
 package chat;
 
+import java.net.*;
+import java.io.*;
+
 public class Server {
     public static void main(String[] args) throws Exception {
         System.out.println("Server started!");
+        String address = "127.0.0.1";
+        int port = 23456;
+        Socket socket = new Socket(InetAddress.getByName(address), port);
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+        System.out.println(input.readUTF());
+        output.writeUTF("Hello from server!");
     }
 }
