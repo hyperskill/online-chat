1c35261ead5e9806d85444fa7041284141353e15
ali-aa <alanplex@mail.ru>
2018-11-29 14:09:41 +0300

    Update Server.java

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -8,10 +8,11 @@ public class Server {
         System.out.println("Server started!");
         String address = "127.0.0.1";
         int port = 23456;
-        Socket socket = new Socket(InetAddress.getByName(address), port);
+        ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
+        Socket socket = server.accept();
         DataInputStream input = new DataInputStream(socket.getInputStream());
         DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
-        System.out.println(input.readUTF());
         output.writeUTF("Hello from server!");
+        System.out.println(input.readUTF());
     }
 }
