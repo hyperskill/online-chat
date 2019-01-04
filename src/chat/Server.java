9c9d68522485676434b48d0b9c2a0d8ab6132680
Kostya <chachabooms@gmail.com>
2019-01-04 06:49:09 +0300

    changed server and client

--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,7 +1,28 @@
 package chat;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
     public static void main(String[] args) throws Exception {
         System.out.println("Server started!");
+        String address = "127.0.0.1";
+        int port = 23456;
+
+        ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(address));
+        Socket socket = server.accept();
+        DataInputStream input = new DataInputStream(socket.getInputStream());
+        DataOutputStream output  = new DataOutputStream(socket.getOutputStream());
+
+        String in = input.readUTF();
+        System.out.println(in);
+        output.writeUTF("Hello from Server!");
+        output.flush();
+
+
     }
 }
