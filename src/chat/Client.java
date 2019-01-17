d0cc1075fed1a81db9f572d7abbcc460a3453e77
Sergei Khmelevskiy <xmelko91@gmail.com>
2019-01-17 22:06:19 +0300

    client-serv-party

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -5,19 +5,36 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
+
         System.out.println("Client started!");
 
         String address = "127.0.0.1";
+        Scanner sc = new Scanner(System.in);
+        int count = -1;
+
         int port = 12345;
         Socket socket = new Socket(InetAddress.getByName(address), port);
         DataInputStream dis = new DataInputStream(socket.getInputStream());
         DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
 
-        dos.writeUTF("Hello from client!");
-        String str = dis.readUTF();
-        System.out.println(str);
+        System.out.println(dis.readUTF());
+
+        String str;
+        while (true) {
+            System.out.print("Type message: ");
+            str = sc.nextLine();
+            count += str.split("\\s+").length;
+            if (str.equals("exit")) {
+                dos.writeUTF(str);
+                break;
+            }
+            dos.writeUTF(str);
+            str = dis.readUTF();
+            System.out.println(str);
+        }
     }
 }
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -2,26 +2,84 @@ package chat;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Server {
     public static void main(String[] args) throws Exception {
         System.out.println("Server started!");
-
+        int Clients = 1;
         String address = "127.0.0.1";
         int port = 12345;
         ServerSocket ss = new ServerSocket(port, 50, InetAddress.getByName(address));
-        Socket socket = ss.accept();
-        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-        DataInputStream dis = new DataInputStream(socket.getInputStream());
 
-        String str = dis.readUTF();
-        dos.writeUTF("Hello from server!");
+        while (true) {
+            Socket socket = ss.accept();
+            Cloent cl = new Cloent(socket, Clients);
+            cl.start();
+            Clients++;
+        }
 
-        System.out.println(str);
 
+
+    }
+}
+
+class Cloent extends Thread{
+    DataInputStream dis;
+    DataOutputStream dos;
+    Socket socket;
+    int count = -1;
+
+    Cloent(Socket socket, int cleintsNb) throws IOException {
+        super("Client-" + cleintsNb);
+        this.socket = socket;
+        this.dis = new DataInputStream(socket.getInputStream());
+        this.dos = new DataOutputStream(socket.getOutputStream());
+        dos.writeUTF("Connected to server. Hello!");
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Client connected.");
+        String str = "";
+        while (!str.equals("exit")){
+            try {
+                str = this.dis.readUTF();
+            } catch (Exception e) {
+                try {
+                    socket.close();
+                    break;
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+                e.printStackTrace();
+            }
+            if (!str.equals("exit")) {
+                System.out.println("Client wrote: " +str + " from " + Thread.currentThread().getName());
+            }
+            try {
+                System.out.println("Send to client: " + "You wrote " + getWordsCount(str) + " word(s).");
+                dos.writeUTF("You wrote " + getWordsCount(str) + " word(s).");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            count += getWordsCount(str);
+        }
+        try {
+            Thread.sleep(1000);
+            socket.close();
+            System.out.println("Client disconnected.");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int getWordsCount(String str){
+        return str.split("\\s+").length;
     }
 }
 
