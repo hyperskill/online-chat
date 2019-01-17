1a5e2072e5ac4d383e8da76e7f5602c69ad4e989
Sergei Khmelevskiy <xmelko91@gmail.com>
2019-01-17 20:34:32 +0300

    1st hello

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,7 +1,23 @@
 package chat;
 
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+
 public class Client {
     public static void main(String[] args) throws Exception {
         System.out.println("Client started!");
+
+        String address = "127.0.0.1";
+        int port = 12345;
+        Socket socket = new Socket(InetAddress.getByName(address), port);
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+
+        dos.writeUTF("Hello from client!");
+        String str = dis.readUTF();
+        System.out.println(str);
     }
 }
--- src/chat/Server.java
+++ src/chat/Server.java
@@ -1,7 +1,41 @@
 package chat;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) throws Exception {
         System.out.println("Server started!");
+
+        String address = "127.0.0.1";
+        int port = 12345;
+        ServerSocket ss = new ServerSocket(port, 50, InetAddress.getByName(address));
+        Socket socket = ss.accept();
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+
+        String str = dis.readUTF();
+        dos.writeUTF("Hello from server!");
+
+        System.out.println(str);
+
     }
 }
+
+
+/*
+    String[] allStr = str.split("\\s+");
+            if (allStr[1].equals("joined") || allStr[1].equals("left") || allStr[1].equals("disconnected") || allStr[1].equals("connected")){
+                    continue;
+                    }
+                    if (allStr[1].equals("sent")){
+                    allStr[1] = ":";
+                    System.out.print(allStr[0]);
+                    for (int i = 1; i < allStr.length; i++){
+        System.out.print(allStr[i] + " ");
+        }
+        System.out.println("");
+        }*/
